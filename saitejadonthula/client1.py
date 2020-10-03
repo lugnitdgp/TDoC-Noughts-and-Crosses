8ba6031a4988612d7acdb988e53fe05cf3ea6364
saitejadonthula <teja.donthula@gmail.com>
2020-10-03 17:22:23 +0530

    first_project

---
+++ saitejadonthula/client1.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+s=socket.socket()
+s.connect(("192.168.137.1",3456))
+print('enter a message, write quit for exit !')
+
+try:
+    while True:
+        data = input('>')
+        s.send(str.encode(data))
+        if data == "quit":
+            s.close()
+            sys.exit()
+        #print('sent !')
+        data = s.recv(1024).decode("utf-8")
+        print(data)
+        if data == "your friend's connection is lost :)":
+            s.close()
+            sys.exit()
+except:
+    print("connection lost")
+
+    
