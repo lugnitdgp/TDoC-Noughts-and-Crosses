222aae625b83e86ee43e5bb0623375007f16f25e
Saikiran727 <60846827+Saikiran727@users.noreply.github.com>
2020-10-04 19:05:45 +0530

    Update client.py

--- Saikiran727/client.py
+++ Saikiran727/client.py
@@ -1,1 +1,12 @@
 
+import socket
+s = socket.socket()
+host = "192.168.137.1"
+port = 10000
+s.connect((host,port))
+while True:
+    msg = str(input())
+    s.send(str.encode(msg))
+    data = s.recv(1024)
+    recieve = data.decode('utf-8')
+s.close()
