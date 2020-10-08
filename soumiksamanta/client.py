6e3acbb888d60b69620e3b2154ec83660cdd3344
soumiksamanta007 <soumiksamantaa@gmail.com>
2020-10-09 00:37:25 +0530

    completed task 3

--- soumiksamanta/client.py
+++ soumiksamanta/client.py
@@ -18,17 +18,189 @@ def create_client():
         time.sleep(2)
         create_client()
 
+def drawBoard(boardMatrix):
+    if boardMatrix[0][0] == -1:
+        print("   |", end="")
+    elif boardMatrix[0][0] == 0:
+        print(" O |", end="")
+    elif boardMatrix[0][0] == 1:
+        print(" X |", end="")
+    
+    if boardMatrix[0][1] == -1:
+        print("   |", end="")
+    elif boardMatrix[0][1] == 0:
+        print(" O |", end="")
+    elif boardMatrix[0][1] == 1:
+        print(" X |", end="")
+    
+    if boardMatrix[0][2] == -1:
+        print("  ")
+    elif boardMatrix[0][2] == 0:
+        print(" O")
+    elif boardMatrix[0][2] == 1:
+        print(" X")
+    
+    print("-----------")
+    
+    if boardMatrix[1][0] == -1:
+        print("   |", end="")
+    elif boardMatrix[1][0] == 0:
+        print(" O |", end="")
+    elif boardMatrix[1][0] == 1:
+        print(" X |", end="")
+    
+    if boardMatrix[1][1] == -1:
+        print("   |", end="")
+    elif boardMatrix[1][1] == 0:
+        print(" O |", end="")
+    elif boardMatrix[1][1] == 1:
+        print(" X |", end="")
+    
+    if boardMatrix[1][2] == -1:
+        print("  ")
+    elif boardMatrix[1][2] == 0:
+        print(" O")
+    elif boardMatrix[1][2] == 1:
+        print(" X")
+    
+    print("-----------")
+    
+    if boardMatrix[2][0] == -1:
+        print("   |", end="")
+    elif boardMatrix[2][0] == 0:
+        print(" O |", end="")
+    elif boardMatrix[2][0] == 1:
+        print(" X |", end="")
+    
+    if boardMatrix[2][1] == -1:
+        print("   |", end="")
+    elif boardMatrix[2][1] == 0:
+        print(" O |", end="")
+    elif boardMatrix[2][1] == 1:
+        print(" X |", end="")
+    
+    if boardMatrix[2][2] == -1:
+        print("  ")
+    elif boardMatrix[2][2] == 0:
+        print(" O")
+    elif boardMatrix[2][2] == 1:
+        print(" X")
+    
+def checkWin(boardMatrix):
+    res = None
+    # check rows
+    if boardMatrix[0][0] == boardMatrix[0][1] and boardMatrix[0][0] == boardMatrix[0][2] and boardMatrix[0][0] != -1:
+        res = boardMatrix[0][0]
+    if boardMatrix[1][0] == boardMatrix[1][1] and boardMatrix[1][0] == boardMatrix[1][2] and boardMatrix[1][0] != -1:
+        res = boardMatrix[0][0]
+    if boardMatrix[2][0] == boardMatrix[2][1] and boardMatrix[2][0] == boardMatrix[2][2] and boardMatrix[2][0] != -1:
+        res = boardMatrix[0][0]
+
+    # check columns
+    if boardMatrix[0][0] == boardMatrix[1][0] and boardMatrix[0][0] == boardMatrix[2][0] and boardMatrix[0][0] != -1:
+        res = boardMatrix[0][0]
+    if boardMatrix[0][1] == boardMatrix[1][1] and boardMatrix[0][1] == boardMatrix[2][1] and boardMatrix[0][1] != -1:
+        res = boardMatrix[0][0]
+    if boardMatrix[0][2] == boardMatrix[1][2] and boardMatrix[0][2] == boardMatrix[2][2] and boardMatrix[0][2] != -1:
+        res = boardMatrix[0][0]
+
+    # check diagonals
+    if boardMatrix[0][0] == boardMatrix[1][1] and boardMatrix[0][0] == boardMatrix[2][2] and boardMatrix[0][0] != -1:
+        res = boardMatrix[0][0]
+    if boardMatrix[0][2] == boardMatrix[1][1] and boardMatrix[0][2] == boardMatrix[2][0] and boardMatrix[0][2] != -1:
+        res = boardMatrix[0][0]
+
+    #checkDraw
+    if res == None and not (any(-1 in row for row in boardMatrix)):
+        return -1
+    return res
+
+
 def communicate():
     global CLIENT
+    print('Welcome to Tic-Tac-Toe CLI\n')
+    print(" 1 | 2 | 3\n-----------\n 4 | 5 | 6\n-----------\n 7 | 8 | 9\n")
+    print('Type (1-9) for placing your move\n')
+    CLIENT.send(bytes('CONNECT', 'utf-8'))
+    incoming_msg = CLIENT.recv(BUFF_SIZE)
+    print(incoming_msg.decode('utf-8'))
+    
+    incoming_msg = CLIENT.recv(BUFF_SIZE)
+    incoming_msg = incoming_msg.decode('utf-8')
+    if incoming_msg == 'QUIT':
+        print('Lost connection with peer!! Quitting...')
+        return
+    print(incoming_msg)
+    player = incoming_msg[-1]
+
+    boardMatrix = [[-1, -1, -1],
+                   [-1, -1, -1],
+                   [-1, -1, -1]]
+    
+    if player == 'X':
+        drawBoard(boardMatrix)
+        self_move = input('Your move > ')
+        boardMatrix[(int(self_move)-1)//3][(int(self_move)-1)%3] = 1
+        CLIENT.send(bytes(self_move, 'utf-8'))
+        drawBoard(boardMatrix)
+
     while True:
-        outgoing_msg = input('Type your message here(press <Enter> to quit) > ')
-        CLIENT.send(bytes(outgoing_msg, 'utf-8'))
-        if outgoing_msg is '':
+        print("Waiting for opponent's move...")
+        peer_move = CLIENT.recv(BUFF_SIZE)
+        if not peer_move:
+            print('Disconnected from server!! Quitting...')
             break
-        incoming_msg = CLIENT.recv(BUFF_SIZE)
-        print(incoming_msg.decode('utf-8'))
+        peer_move = peer_move.decode('utf-8')
+        if peer_move == '[GAME OVER]':
+            peer_move = CLIENT.recv(BUFF_SIZE)
+            if player == 'X':
+                boardMatrix[(int(peer_move)-1)//3][(int(peer_move)-1)%3] = 0
+            elif player == 'O':
+                boardMatrix[(int(peer_move)-1)//3][(int(peer_move)-1)%3] = 1
+            drawBoard(boardMatrix)
+            print('[GAME OVER]')
+            res = checkWin(boardMatrix)
+            if res == -1:
+                print('----DRAW----')
+                break
+            print('Oops!! You Lost!!')
+            break
+        elif peer_move == 'QUIT':
+            print('Lost connection with peer!! Quitting...')
+            break
+
+        if player == 'X':
+            boardMatrix[(int(peer_move)-1)//3][(int(peer_move)-1)%3] = 0
+        elif player == 'O':
+            boardMatrix[(int(peer_move)-1)//3][(int(peer_move)-1)%3] = 1
+        drawBoard(boardMatrix)
+        
+        self_move = input('Your move > ')
+        if player == 'X':
+            boardMatrix[(int(self_move)-1)//3][(int(self_move)-1)%3] = 1
+        elif player == 'O':
+            boardMatrix[(int(self_move)-1)//3][(int(self_move)-1)%3] = 0
+        drawBoard(boardMatrix)
+        res = checkWin(boardMatrix)
+
+        if res == None:
+            CLIENT.send(bytes(self_move, 'utf-8'))
+        elif res == -1:
+            CLIENT.send(bytes('[GAME OVER]', 'utf-8'))
+            print('[GAME OVER]')
+            print('----DRAW----')
+            CLIENT.send(bytes(self_move, 'utf-8'))
+            break
+        else:
+            CLIENT.send(bytes('[GAME OVER]', 'utf-8'))
+            print('[GAME OVER]')
+            print('Congrats!! You Win!!')            
+            CLIENT.send(bytes(self_move, 'utf-8'))
+            break
 
 
 if __name__ == '__main__':
     create_client()
     communicate()
+    CLIENT.close()
+    
--- soumiksamanta/server.py
+++ soumiksamanta/server.py
@@ -3,54 +3,110 @@
 import socket
 import threading
 
-HOST = ''
+HOST = socket.gethostname()
 PORT = 9989
 SERVER = None
 BUFF_SIZE = 1024
 THREAD_LOCK = threading.Lock()
+CLIENT_CONNECTIONS = None
+CONNECTED_TO = None
 
 def create_server():
     global SERVER
     try:
         SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("SERVER created successfully")
-    except:
-        print("Socket Creation Error!!")
+    except Exception as e:
+        print("Socket Creation Error!!\n", e)
+        exit()
 
 def bind_server_and_listen():
-    global SERVER
+    global SERVER, HOST, PORT, CLIENT_CONNECTIONS, CONNECTED_TO
     try:
-        SERVER.bind((socket.gethostname(), PORT))
+        SERVER.bind((HOST, PORT))
         SERVER.listen(5)
+        CLIENT_CONNECTIONS = []
+        CONNECTED_TO = []
         print("SERVER listening for connections...")
-    except:
-        print("Socket Binding Error!!")
-        #bind_server_and_listen()
+    except Exception as e:
+        print("Socket Binding Error!!\n", e)
+        exit()
 
 def accept_connections():
-    global SERVER
+    global SERVER, CLIENT_CONNECTIONS
     while True:
         connection, address = SERVER.accept()
         print("[NEW Connection] Connection established with {} on port {}".format(address[0], address[1]))
+        THREAD_LOCK.acquire()
+        CLIENT_CONNECTIONS.append(connection)
+        CONNECTED_TO.append(-1)
         threading.Thread(target=handle_client, args=(connection, address)).start()
+        THREAD_LOCK.release()
 
 def handle_client(conn, addr):
     incoming_msg = conn.recv(BUFF_SIZE)
     incoming_msg = incoming_msg.decode('utf-8')
-    
-    if incoming_msg is not '':
-        conn.send(bytes('Congrats!! You are now connected to the server...\nYou sent : ' + incoming_msg, 'utf-8'))
-        print(f'Message from Client {addr[0]}:{addr[1]} > ', incoming_msg)
-        THREAD_LOCK.acquire()
+    self_index = CLIENT_CONNECTIONS.index(conn)
+    peer_index = -1
+    peer_conn = None
+    connected_to_peer = False
+
+    if incoming_msg == 'CONNECT':
+        conn.send(bytes('Congrats!! You are now connected to the server!\nWaiting for an opponent...', 'utf-8'))      
+        
         while True:
-            incoming_msg = conn.recv(BUFF_SIZE)
-            incoming_msg = incoming_msg.decode('utf-8')
-            if incoming_msg is '':
-                THREAD_LOCK.release()
+            THREAD_LOCK.acquire()
+            if not connected_to_peer:
+                if CONNECTED_TO[self_index] == -1:                
+                    try:                        
+                        peer_index = CONNECTED_TO.index(-1)
+                        if peer_index == self_index:
+                            peer_index = CONNECTED_TO.index(-1, self_index+1)
+                        try:
+                            conn.send(bytes('You are player X', 'utf-8'))
+                        except ConnectionResetError:
+                            break                        
+                        CONNECTED_TO[self_index] = peer_index
+                        CONNECTED_TO[peer_index] = self_index
+                        peer_conn = CLIENT_CONNECTIONS[peer_index]
+                        connected_to_peer = True
+                        
+                        peer_conn.send(bytes('You are player O', 'utf-8'))
+                    except ValueError:
+                        THREAD_LOCK.release()
+                        continue
+                else:
+                    peer_index = CONNECTED_TO[self_index]
+                    peer_conn = CLIENT_CONNECTIONS[peer_index]
+                    connected_to_peer = True
+            THREAD_LOCK.release()
+
+            self_move = conn.recv(BUFF_SIZE)
+            if not self_move:
+                try:
+                    peer_conn.send(bytes('QUIT', 'utf-8'))
+                except OSError:
+                    pass
+                break
+            elif self_move.decode('utf-8') == '[GAME OVER]':
+                peer_conn.send(self_move)
+                self_move = conn.recv(BUFF_SIZE)
+                peer_conn.send(self_move)
                 break
             else:
-                conn.send(bytes('You sent : ' + incoming_msg, 'utf-8'))
-                print(f'Message from Client {addr[0]}:{addr[1]} > ', incoming_msg)
+                peer_conn.send(self_move)
+
+    THREAD_LOCK.acquire()
+    try:
+        self_index = CLIENT_CONNECTIONS.index(conn)
+        del CLIENT_CONNECTIONS[self_index]
+        del CONNECTED_TO[self_index]
+        peer_index = CLIENT_CONNECTIONS.index(peer_conn)
+        del CLIENT_CONNECTIONS[peer_index]
+        del CONNECTED_TO[peer_index]
+    except ValueError:
+        pass
+    THREAD_LOCK.release()
 
     conn.close()
     print(f"[Closed Connection] Connection closed for {addr[0]} on port {addr[1]}")
@@ -59,3 +115,4 @@ if __name__ == '__main__':
     create_server()
     bind_server_and_listen()
     accept_connections()
+    SERVER.close()
